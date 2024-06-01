e205c98eb6e75c41f7f695a694bb509ebb0f5a36
danieleriva03@gmail.com <danieleriva03@gmail.com>
2024-06-02 01:16:22 +0200

    Mobile header. Mobile navbar. Modificata sezione contatti per dispositivi mobile.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 window.onscroll = function () { headerOnScroll() };
+window.onload = function () { resizeHeaderDivider() };
 
 function headerOnScroll() {
     if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
@@ -6,6 +7,24 @@ function headerOnScroll() {
     } else {
         document.getElementsByClassName("header")[0].classList.remove("header-onscroll");
     }
+}
+
+function resizeHeaderDivider() {
+    var headerHeight = document.getElementById('nav-mobile').offsetHeight;
+    document.getElementById('nav-mobile-spacer').style.height = (headerHeight) + "px";
+    document.getElementById("header-mobile").style.marginTop = (headerHeight) + "px";
+}
+
+function hamburgerBtn() {
+    document.getElementById("header-mobile").classList.toggle("mobile-header-visible");
+    document.getElementById("header-mobile").classList.toggle("mobile-header-hidden");
+    document.getElementById("close-header").classList.toggle("mobile-header-visible");
+    document.getElementById("close-header").classList.toggle("mobile-header-hidden");
+
+    if (document.getElementById("header-mobile").classList.contains("mobile-header-visible"))
+        document.getElementById("hamburger-btn").src = "imgs/header/hamburger_close.svg";
+    else
+        document.getElementById("hamburger-btn").src = "imgs/header/hamburger_open.svg";
 }
 
 document.addEventListener('DOMContentLoaded', () => {
